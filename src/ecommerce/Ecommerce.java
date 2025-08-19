fac60a4070df4d8723cbaf2f9f7fd331062221df
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-08-18 22:01:05 -0500

    Semana 02: Implementación de procedimiento buscarProductoPorId, búsqueda lineal para encontrar un productor por su ID.

--- src/ecommerce/Ecommerce.java
+++ src/ecommerce/Ecommerce.java
@@ -10,19 +10,39 @@ package ecommerce;
  */
 public class Ecommerce {
 
-        public static boolean buscarElemento(int [] lista, int elemento){
-            for (int i : lista){
-                if (i == elemento){
-                    return true;
-                }
+    public static Producto[] catalogo = {
+        new Producto(101, "Laptop Gamer"),
+        new Producto(102, "Teclado Mecánico"),
+        new Producto(103, "Monitor"),
+        new Producto(104, "Mouse Inalambrico"),
+        new Producto(105, "Auriculares"),};
+
+    public static Producto buscarProductoPorId(int id) {
+        for (Producto p : catalogo) {
+            if (p.getId() == id) {
+                return p;
             }
-            return false;
         }
-        
-        public static void main(String[] args){
-            int [] arreglo = { 10, 25, 50, 75, 100};
-            int elemento = 50;
-            boolean encontrado = buscarElemento(arreglo, elemento);
-            System.out.println("¿Se encuentra el numero "+ elemento + " en el arreglo? "+ encontrado);       
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Buscando producto con ID 103...");
+        Producto productoEncontrado = buscarProductoPorId(103);
+
+        if (productoEncontrado != null) {
+            System.out.println("Producto encontrado: " + productoEncontrado.toString());
+        } else {
+            System.out.println("Producto no encontrado.");
+        }
+
+        System.out.println("\nBuscando producto con ID 999...");
+        Producto productoNoEncontrado = buscarProductoPorId(999);
+
+        if (productoNoEncontrado != null) {
+            System.out.println("Producto encontrado: " + productoNoEncontrado.toString());
+        } else {
+            System.out.println("Producto no encontrado.");
         }
     }
+}
