20fcff98adc53ff4cfaa8488bb17e410f36f0b09
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-09-06 22:33:05 -0500

    Semana 04: Se añade método para ordenar catálogo por precio

--- src/ecommerce/Ecommerce.java
+++ src/ecommerce/Ecommerce.java
@@ -4,10 +4,8 @@
  */
 package ecommerce;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 
 /**
  *
@@ -31,26 +29,26 @@ public class Ecommerce {
      */
     //Simulación de catálogo desordenado para probar el algoritmo de ordenamiento
     public static Producto[] Lista = {
-        new Producto(103, "Laptop Gamer"),
-        new Producto(105, "Teclado Mecanico"),
-        new Producto(101, "Monitor"),
-        new Producto(102, "Mouse Inalambrico"),
-        new Producto(104, "Auriculares"),
-        new Producto(202, "Silla de Escritorio Ergonomica"),
-        new Producto(204, "Disco Duro Externo"),
-        new Producto(201, "Webcam HD"),
-        new Producto(203, "Micrófono USB"),
-        new Producto(205, "Tarjeta Grafica RTX 4070"),
-        new Producto(305, "Fuente de Poder 850W"),
-        new Producto(302, "Memoria RAM 16GB"),
-        new Producto(303, "Procesador Intel i9"),
-        new Producto(304, "SSD NVMe 1TB"),
-        new Producto(301, "Refrigeracion Liquida"),
-        new Producto(401, "Mousepad XL"),
-        new Producto(404, "Adaptador HDMI"),
-        new Producto(403, "Cable USB-C"),
-        new Producto(405, "Estabilizador"),
-        new Producto(402, "USB 32 GB")
+        new Producto(103, "Laptop Gamer", 3500.00),
+        new Producto(105, "Teclado Mecanico", 149.99),
+        new Producto(101, "Monitor",350.50),
+        new Producto(102, "Mouse Inalambrico", 99.99),
+        new Producto(104, "Auriculares", 129.00),
+        new Producto(202, "Silla de Escritorio Ergonomica", 1200.00),
+        new Producto(204, "Disco Duro Externo", 199.99),
+        new Producto(201, "Webcam HD", 250.00),
+        new Producto(203, "Micrófono USB", 125.50),
+        new Producto(205, "Tarjeta Grafica RTX 4070", 3990.90),
+        new Producto(305, "Fuente de Poder 850W", 150.00),
+        new Producto(302, "Memoria RAM 16GB", 175.50),
+        new Producto(303, "Procesador Intel i9", 780.00),
+        new Producto(304, "SSD NVMe 1TB", 670.00),
+        new Producto(301, "Refrigeracion Liquida", 250.00),
+        new Producto(401, "Mousepad XL", 90.90),
+        new Producto(404, "Adaptador HDMI", 14.99),
+        new Producto(403, "Cable USB-C", 25.50),
+        new Producto(405, "Estabilizador", 180.00),
+        new Producto(402, "USB 32 GB", 39.99)
     };
 
     public static Producto[] catalogo = Arrays.copyOf(Lista, Lista.length);
@@ -63,6 +61,10 @@ public class Ecommerce {
         return catalogo;
     }
 
+    public static void ordenarCatalogoPorPrecio() {
+        OrdenacionInterna.ordenarPorPrecio(catalogo);
+    }
+    
     public static void ordenarCatalogoPorId() {
         OrdenacionInterna.ordenarPorInsercion(catalogo);
     }
