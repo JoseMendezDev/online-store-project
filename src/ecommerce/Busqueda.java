410b0517bde627fc3efc1166bc69204e83eedbd1
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-09-09 21:48:48 -0500

    Semana 04: Se implementa busqueda binaria

--- src/ecommerce/Busqueda.java
+++ src/ecommerce/Busqueda.java
@@ -9,11 +9,28 @@ package ecommerce;
  * Clase que contiene todos los algoritmos de busqueda.
  */
 public class Busqueda {
-    
-        public static Producto buscarLineal(Producto[] catalogo, int id) {
+
+    public static Producto buscarLineal(Producto[] catalogo, int id) {
         for (Producto p : catalogo) {
             if (p.getId() == id) {
                 return p;
+            }
+        }
+        return null;
+    }
+
+    public static Producto buscarBinaria(Producto[] catalogo, int id) {
+        int izquierda = 0;
+        int derecha = catalogo.length - 1;
+        while (izquierda <= derecha) {
+            int medio = izquierda + (derecha - izquierda) / 2;
+            if (catalogo[medio].getId() == id) {
+                return catalogo[medio];
+            }
+            if (catalogo[medio].getId() < id) {
+                izquierda = medio + 1;
+            } else {
+                derecha = medio - 1;
             }
         }
         return null;
