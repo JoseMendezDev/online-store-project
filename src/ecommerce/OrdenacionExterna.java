7062e87811e6fc1e39454dbae7ac2fde8e04f10f
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-09-17 21:51:36 -0500

    Semana 06: Se actualiza clase de Ordenación Externa

--- src/ecommerce/OrdenacionExterna.java
+++ src/ecommerce/OrdenacionExterna.java
@@ -5,17 +5,17 @@
 package ecommerce;
 
 import java.util.*;
-import java.io.*;
 
 /**
  *
- * Clase que contiene todos los algoritmos de ordenación externa (fuera de memoria).
+ * Clase que contiene todos los algoritmos de ordenación externa (fuera de
+ * memoria).
  */
 public class OrdenacionExterna {
 
-    public static void ordenarPorFusionNatural(Producto[] catalogo) {
-        int n = catalogo.length;
-        Producto[] tempArray = new Producto[n];
+    public static void ordenarPorFusionNatural(ArrayList<Producto> catalogo) {
+        int n = catalogo.size();
+        ArrayList<Producto> tempArray = new ArrayList<>(Collections.nCopies(n, null));
 
         boolean ordenado = false;
         while (!ordenado) {
@@ -23,39 +23,41 @@ public class OrdenacionExterna {
             int i = 0;
             while (i < n) {
                 int j = i;
-                while (j < n - 1 && catalogo[j].getId() <= catalogo[j + 1].getId()) {
+                while (j < n - 1 && catalogo.get(j).getId() <= catalogo.get(j + 1).getId()) {
                     j++;
                 }
                 if (j == n - 1) {
                     break;
                 }
                 int k = j + 1;
-                while (k < n - 1 && catalogo[k].getId() <= catalogo[k + 1].getId()) {
+                while (k < n - 1 && catalogo.get(k).getId() <= catalogo.get(k + 1).getId()) {
                     k++;
                 }
                 ordenado = false;
+
                 int p1 = i, p2 = j + 1, p3 = i;
                 while (p1 <= j && p2 <= k) {
-                    if (catalogo[p1].getId() <= catalogo[p2].getId()) {
-                        tempArray[p3++] = catalogo[p1++];
+                    if (catalogo.get(p1).getId() <= catalogo.get(p2).getId()) {
+                        tempArray.set(p3++, catalogo.get(p1++));
                     } else {
-                        tempArray[p3++] = catalogo[p2++];
+                        tempArray.set(p3++, catalogo.get(p2++));
                     }
                 }
                 while (p1 <= j) {
-                    tempArray[p3++] = catalogo[p1++];
+                    tempArray.set(p3++, catalogo.get(p1++));
                 }
                 while (p2 <= k) {
-                    tempArray[p3++] = catalogo[p2++];
+                    tempArray.set(p3++, catalogo.get(p2++));
                 }
+
                 for (int l = i; l <= k; l++) {
-                    catalogo[l] = tempArray[l];
+                    catalogo.set(l, tempArray.get(l));
                 }
                 i = k + 1;
             }
         }
     }
-    
+
     public static List<List<Integer>> crearCorridas(List<Integer> datos, int tamBloque) {
         List<List<Integer>> corridas = new ArrayList<>();
         for (int i = 0; i < datos.size(); i += tamBloque) {
@@ -64,11 +66,10 @@ public class OrdenacionExterna {
             Collections.sort(ordenado);
             corridas.add(ordenado);
         }
-        return corridas;}
-        
-        
+        return corridas;
+    }
 
-/*    public static List<Integer> mezclaEquilibrada(List<List<Integer>> corridas) {
+    /*    public static List<Integer> mezclaEquilibrada(List<List<Integer>> corridas) {
         Queue<List<Integer>> cola = new LinkedList<>(corridas);
         while (cola.size() > 1) {
             List<Integer> corrida1 = cola.poll();
@@ -78,10 +79,10 @@ public class OrdenacionExterna {
         return cola.poll();
     }
     
-*/
+     */
     public static List<Integer> metodoPolifasico(List<List<Integer>> corridas) {
         Queue<List<Integer>> a = new LinkedList<>();
         Queue<List<Integer>> b = new LinkedList<>();
         return null;
     }
-} 
+}
