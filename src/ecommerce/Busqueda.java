30d67652f578cb19b54810f1ef6e20b08ffaea70
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-09-17 21:52:01 -0500

    Semana 06: Se actualiza clase de Busqueda

--- src/ecommerce/Busqueda.java
+++ src/ecommerce/Busqueda.java
@@ -4,13 +4,15 @@
  */
 package ecommerce;
 
+import java.util.ArrayList;
+
 /**
  *
  * Clase que contiene todos los algoritmos de busqueda.
  */
 public class Busqueda {
 
-    public static Producto buscarLineal(Producto[] catalogo, int id) {
+    public static Producto buscarLineal(ArrayList<Producto> catalogo, int id) {
         for (Producto p : catalogo) {
             if (p.getId() == id) {
                 return p;
@@ -19,17 +21,21 @@ public class Busqueda {
         return null;
     }
 
-    public static Producto buscarBinaria(Producto[] catalogo, int id) {
+public static Producto buscarBinaria(ArrayList<Producto> catalogo, int id) {
         int izquierda = 0;
-        int derecha = catalogo.length - 1;
+        int derecha = catalogo.size() - 1;
+        
         while (izquierda <= derecha) {
             int medio = izquierda + (derecha - izquierda) / 2;
-            if (catalogo[medio].getId() == id) {
-                return catalogo[medio];
+            
+            if (catalogo.get(medio).getId() == id) {
+                return catalogo.get(medio);
             }
-            if (catalogo[medio].getId() < id) {
+            
+            if (catalogo.get(medio).getId() < id) {
                 izquierda = medio + 1;
-            } else {
+            } 
+            else {
                 derecha = medio - 1;
             }
         }
