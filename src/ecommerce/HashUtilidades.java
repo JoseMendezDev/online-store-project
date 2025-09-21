eddc3abe524b57cbedd2c6a188fcedabb06951f2
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-09-20 20:57:51 -0500

    Semana 06: Se actualiza clase Hash Utilidades y se crea metodo Generar Hash

--- src/ecommerce/HashUtilidades.java
+++ src/ecommerce/HashUtilidades.java
@@ -4,10 +4,29 @@
  */
 package ecommerce;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  *
  * @author USER
  */
 public class HashUtilidades {
-    
+
+    public static String generarHash(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            byte[] hashBytes = md.digest(password.getBytes());
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashBytes) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Error al generar el hash: " + e.getMessage());
+        }
+    }
 }
