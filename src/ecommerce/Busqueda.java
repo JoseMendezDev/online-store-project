0899df08d3b3b34d48eb4b0ba2d7bd99e557193d
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-09-23 22:37:44 -0500

    Semana 06: Se actualiza clase búsqueda

--- src/ecommerce/Busqueda.java
+++ src/ecommerce/Busqueda.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
  */
 public class Busqueda {
 
-    public static Producto buscarLineal(ArrayList<Producto> catalogo, int codigo) {
+    public static Producto buscarLineal(ArrayList<Producto> catalogo, String codigo) {
         for (Producto p : catalogo) {
             if (p.getCodigo() == codigo) {
                 return p;
@@ -21,18 +21,19 @@ public class Busqueda {
         return null;
     }
 
-public static Producto buscarBinaria(ArrayList<Producto> catalogo, int codigo) {
+public static Producto buscarBinaria(ArrayList<Producto> catalogo, String codigo) {
         int izquierda = 0;
         int derecha = catalogo.size() - 1;
         
         while (izquierda <= derecha) {
             int medio = izquierda + (derecha - izquierda) / 2;
+            int comparacion = catalogo.get(medio).getCodigo().compareTo(codigo);
             
-            if (catalogo.get(medio).getCodigo() == codigo) {
+            if (comparacion == 0) {
                 return catalogo.get(medio);
             }
             
-            if (catalogo.get(medio).getCodigo() < codigo) {
+            if (comparacion < 0) {
                 izquierda = medio + 1;
             } 
             else {
