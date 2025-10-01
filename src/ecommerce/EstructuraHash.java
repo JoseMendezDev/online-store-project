45f92ea993e29de5935d08aca9ab11d88b11ec92
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-09-30 21:49:05 -0500

    Semana 07: Manejo de colisiones en la clase estructura hash

--- src/ecommerce/EstructuraHash.java
+++ src/ecommerce/EstructuraHash.java
@@ -5,32 +5,73 @@
 package ecommerce;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
 
 /**
  *
  * @author USER
  */
-public class EstructuraHash {
+public class EstructuraHash
+{
 
-    private static HashMap<String, Producto> productoMap = new HashMap<>();
+    private static final int TAMANO_TABLA = 10;
+    private static List<Producto>[] tabla;
 
-    public static void inicializar(ArrayList<Producto> catalogo) {
-        productoMap.clear();
-        for (Producto p : catalogo) {
-            productoMap.put(p.getCodigo(), p);
+    public static void inicializar(ArrayList<Producto> catalogoInicial)
+    {
+        tabla = new List[TAMANO_TABLA];
+        for (int i = 0; i < TAMANO_TABLA; i++)
+        {
+            tabla[i] = new ArrayList<>();
+        }
+
+        for (Producto p : catalogoInicial)
+        {
+            agregarProducto(p);
         }
     }
 
-    public static void agregarProducto(Producto producto) {
-        productoMap.put(producto.getCodigo(), producto);
-    }
-    
-    public static boolean existeCodigo(String codigo){
-        return productoMap.containsKey(codigo);
+    private static int funcionHash(String codigo)
+    {
+        if (codigo == null || codigo.length() < 6)
+        {
+            return 0;
+        }
+        try
+        {
+            String subString = codigo.substring(4, 6);
+            int valor = Integer.parseInt(subString);
+
+            return valor % TAMANO_TABLA;
+        } catch (NumberFormatException e)
+        {
+            return codigo.hashCode() % TAMANO_TABLA;
+        }
     }
 
-    public static Producto buscarProducto(String codigo) {
-        return productoMap.get(codigo);
+    public static void agregarProducto(Producto producto)
+    {
+        int indice = funcionHash(producto.getCodigo());
+
+        tabla[indice].add(producto);
+    }
+
+    public static Producto buscarProducto(String codigo)
+    {
+        int indice = funcionHash(codigo);
+
+        for (Producto p : tabla[indice])
+        {
+            if (p.getCodigo().equals(codigo))
+            {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public static boolean existeCodigo(String codigo)
+    {
+        return buscarProducto(codigo) != null;
     }
 }
