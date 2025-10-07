007ef238cc9c4712effd78f0423970d1009a249d
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-10-06 22:19:00 -0500

    Semana 08: Se implementa atributos, constructor y getters and setters a la clase cliente.

--- src/ecommerce/Cliente.java
+++ src/ecommerce/Cliente.java
@@ -4,11 +4,73 @@
  */
 package ecommerce;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
- * @author USER
+ * Clase que representa un cliente
  */
 public class Cliente
 {
+    private String id;
+    private String nombre;
+    private String email;
+    private String direccion;
+    private String telefono;
+    private List<String> historialCompras;
+    private LocalDateTime fechaRegistro;
+
+    public Cliente(String id, String nombre, String email, String direccion)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+        this.direccion = direccion;
+        this.historialCompras = new ArrayList<>();
+        this.fechaRegistro = LocalDateTime.now();
+    }
     
+    public String getId()
+    {
+        return id;
+    }
+
+    public String getNombre()
+    {
+        return nombre;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public String getDireccion()
+    {
+        return direccion;
+    }
+
+    public String getTelefono()
+    {
+        return telefono;
+    }
+
+    public List<String> getHistorialCompras()
+    {
+        return historialCompras;
+    }
+    
+    public void setTelefono(String telefono)
+    {
+        this.telefono = telefono;
+    }
+
+    public void agregarCompra(String pedidoId)
+    {
+        historialCompras.add(pedidoId);
+    }
 }
+
+
