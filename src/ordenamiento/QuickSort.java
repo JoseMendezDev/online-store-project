7c336d74a921d98d3555941fe028a0927df36527
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-10-22 21:42:19 -0500

    Semana 11: Se añade comportamientos de la clase QuickSort

--- src/ordenamiento/QuickSort.java
+++ src/ordenamiento/QuickSort.java
@@ -4,10 +4,55 @@
  */
 package ordenamiento;
 
+import java.util.ArrayList;
+import negocio.Producto;
+
 /**
  *
  * @author USER
  */
 public class QuickSort {
+    public static void ordenar(ArrayList<Producto> A) {
+
+        ArrayList<Producto> listaOrdenada = quickSortRecursive(A);
+        
+        A.clear();
+        A.addAll(listaOrdenada);
+    }
     
+    private static ArrayList<Producto> quickSortRecursive(ArrayList<Producto> A) {
+        int n = A.size();
+
+        if (n <= 1) {
+            return A;
+        }
+
+        Producto P = A.get(0);
+
+        ArrayList<Producto> L = new ArrayList<>(); // Menores que P
+        ArrayList<Producto> E = new ArrayList<>(); // Iguales a P
+        ArrayList<Producto> G = new ArrayList<>(); // Mayores que P
+        
+        for (Producto producto : A) {
+            int comparacion = producto.getCodigo().compareTo(P.getCodigo());
+            
+            if (comparacion < 0) {
+                L.add(producto);
+            } else if (comparacion > 0) {
+                G.add(producto);
+            } else {
+                E.add(producto);
+            }
+        }
+
+        ArrayList<Producto> L_sorted = quickSortRecursive(L);
+        ArrayList<Producto> G_sorted = quickSortRecursive(G);
+
+        ArrayList<Producto> resultado = new ArrayList<>();
+        resultado.addAll(L_sorted);
+        resultado.addAll(E);
+        resultado.addAll(G_sorted);
+        
+        return resultado;
+    }  
 }
