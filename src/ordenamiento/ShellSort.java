7c770333a05c89952aeb570834d84d35c57b76eb
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-10-22 21:42:30 -0500

    Semana 11: Se añade comportamientos de la clase ShellSort

--- src/ordenamiento/ShellSort.java
+++ src/ordenamiento/ShellSort.java
@@ -4,10 +4,38 @@
  */
 package ordenamiento;
 
+import utilidades.Utilidades;
+import java.util.ArrayList;
+import negocio.Producto;
+
 /**
+ * Ordena el ArrayList de Productos usando ShellSort.
  *
  * @author USER
  */
 public class ShellSort {
-    
+
+    public static void ordenar(ArrayList<Producto> A) {
+        int n = A.size();
+
+        int h = 1;
+        while (h < n / 3) {
+            h = 3 * h + 1;
+        }
+
+        while (h >= 1) {
+            for (int i = h; i < n; i++) {
+
+                int j = i;
+
+                while (j >= h && A.get(j).getCodigo().compareTo(A.get(j - h).getCodigo()) < 0) {
+
+                    Utilidades.intercambiar(A, j, j - h);
+
+                    j = j - h;
+                }
+            }
+            h = h / 3;
+        }
+    }
 }
