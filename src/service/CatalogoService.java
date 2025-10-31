44b21a9124123cc27bebdb4685006d85846ed2ad
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-10-30 20:08:12 -0500

    Se añade nuevo paquete de servicios para limpiar código

---
+++ src/service/CatalogoService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package service;
+
+/**
+ * Servicio centralizado para la gestión del catálogo de productos.
+ *
+ * @author USER
+ */
+public class CatalogoService {
+}
