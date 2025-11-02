3462e79d8e0b64ce2e61cfc418d8d6efaf9e515c
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-11-01 23:47:02 -0500

    Semana 12: Se añade metodos de excepcion stock insuficiente

--- src/excepciones/StockInsuficienteException.java
+++ src/excepciones/StockInsuficienteException.java
@@ -5,9 +5,32 @@
 package excepciones;
 
 /**
- *
+ * Cuando hay problemas con el stock
  * @author USER
  */
-public class StockInsuficienteException {
-    
+public class StockInsuficienteException extends EcommerceException {
+
+    private final String codigoProducto;
+    private final int stockDisponible;
+    private final int stockSolicitado;
+
+    public StockInsuficienteException(String codigoProducto, int stockDisponible, int stockSolicitado) {
+        super(String.format("Stock insuficiente para producto %s. Disponible: %d, Solicitado: %d",
+                codigoProducto, stockDisponible, stockSolicitado));
+        this.codigoProducto = codigoProducto;
+        this.stockDisponible = stockDisponible;
+        this.stockSolicitado = stockSolicitado;
+    }
+
+    public String getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public int getStockDisponible() {
+        return stockDisponible;
+    }
+
+    public int getStockSolicitado() {
+        return stockSolicitado;
+    }
 }
