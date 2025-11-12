8ac01a2a88008962b6722fe76dd0aa2b4b74c256
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-11-11 22:24:51 -0500

    Semana 13: Se hace refactor a clase QuickSort

--- src/algoritmos/ordenamiento/QuickSort.java
+++ src/algoritmos/ordenamiento/QuickSort.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ordenamiento;
+package algoritmos.ordenamiento;
 
 import java.util.ArrayList;
 import domain.Producto;
