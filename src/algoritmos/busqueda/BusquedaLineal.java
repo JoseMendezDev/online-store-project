cef47a575af553f18c89ff96969e00ed54a94ff5
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-11-11 22:11:35 -0500

    Semana 13: Se traslada método de búsqueda lineal a paquete búsquedas

--- src/algoritmos/busqueda/BusquedaLineal.java
+++ src/algoritmos/busqueda/BusquedaLineal.java
@@ -4,10 +4,27 @@
  */
 package algoritmos.busqueda;
 
+import domain.Producto;
+import java.util.ArrayList;
+
 /**
  *
  * @author USER
  */
 public class BusquedaLineal {
-    
+     /*
+     * Búsqueda Lineal - O(n)
+     */
+    public static Producto buscarLineal(ArrayList<Producto> catalogo, String codigo) {
+        if (catalogo == null || codigo == null) {
+            return null;
+        }
+
+        for (Producto p : catalogo) {
+            if (codigo.equals(p.getCodigo())) {
+                return p;
+            }
+        }
+        return null;
+    }
 }
