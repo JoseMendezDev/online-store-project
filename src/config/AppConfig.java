fb78665acf5c7e1c14705693ee3d110ce4a2182f
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-11-12 22:01:29 -0500

    Semana 14: Se añade clase app config y se crean sus metodos y atributos.

---
+++ src/config/AppConfig.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package config;
+
+/**
+ *
+ * @author USER
+ */
+public class AppConfig {
+    
+    // Base de datos / Archivos
+    public static final String ARCHIVO_CATALOGO = "catalogo_productos.txt";
+    
+    // Paginación
+    public static final int PRODUCTOS_POR_PAGINA = 30;
+    
+    // Hash Table
+    public static final int TAMANO_TABLA_HASH = 10;
+    
+    // Ordenación Externa
+    public static final int MEMORIA_MAXIMA_ORDENACION = 100;
+    
+    // UI
+    public static final int ANCHO_VENTANA = 1100;
+    public static final int ALTO_VENTANA = 700;
+    
+    // Autenticación
+    public static final String USUARIO_ADMIN = "admin";
+    public static final String HASH_PASSWORD_ADMIN = "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f";
+    
+    // Versión
+    public static final String VERSION = "2.0";
+    public static final String NOMBRE_APP = "E-commerce System";
+    
+    private AppConfig() {
+    }
+}
