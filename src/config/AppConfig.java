50fe78efe10b144de3b4d06ec5e8cf9e976fd008
JoseMendezDev <josemendez.jlm.jm@gmail.com>
2025-11-14 21:19:11 -0500

    Semana 14: modificacion clase app config

--- src/config/AppConfig.java
+++ src/config/AppConfig.java
@@ -31,7 +31,7 @@ public class AppConfig {
     public static final String HASH_PASSWORD_ADMIN = "ef92b778bafe771e89245b89ecbc08a44a4e166c06659911881f383d4473e94f";
     
     // Versión
-    public static final String VERSION = "2.0";
+    public static final String VERSION = "1.0";
     public static final String NOMBRE_APP = "E-commerce System";
     
     private AppConfig() {
